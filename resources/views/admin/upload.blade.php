d262db929809a74a39e58349fcbb28caced1813b
manishdixit18 <manishdixit18@gmail.com>
2017-09-25 17:54:18 +0530

    upload

--- resources/views/admin/upload.blade.php
+++ resources/views/admin/upload.blade.php
@@ -7,7 +7,12 @@
                 	<form method="post" action="{{URL::to('upload-file')}}" enctype="multipart/form-data">
                 		{{csrf_field()}}
                 	<h1 class="page-header">Upload your policies here</h1>
-                    <div class="form-group"><label  class="col-sm-2 col-form-label">Choose File</label><input type="file" name="file[]" class="col-sm-10" required><input type="date" name="expiry_date" class="form-control "><a class="upload btn btn-primary">upload</a></div>
+                    <div class="form-group">
+                      <label  class="col-sm-2 col-form-label">Choose File</label>
+                      <input type="file" name="file[]" class="col-sm-10" required>
+                      <input type="date" name="expiry_date" class="form-control ">
+                      <a class="upload btn btn-primary">upload</a>
+                    </div>
                     <a href="#" class="add-more btn btn-primary col-sm-12" >Add More</a>
                 	</form>
                     
