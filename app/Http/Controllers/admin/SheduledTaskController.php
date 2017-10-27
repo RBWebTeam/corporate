d07dc962f06c8ce005d0cf1a8b1ef3885cae1f18
manishdixit18 <manishdixit18@gmail.com>
2017-10-27 11:20:51 +0530

    intimation mail pdf part

--- app/Http/Controllers/admin/SheduledTaskController.php
+++ app/Http/Controllers/admin/SheduledTaskController.php
@@ -10,7 +10,24 @@ use Mail;
 class SheduledTaskController extends Controller
 {
 	public function sendIntiamtionMail(Request $req){
-
+		$emp_code="";
+		$error=0;
+		$all_error_msg=[];
+		$managers=DB::select('CALL usp_load_managers()');
+		foreach ($managers as $key => $emp_code) {
+			try{	
+					$data=$this::get_managerwise_data($emp_code->empcode);
+					$status=$this::sendMail();
+				}catch(\Exception $ee){
+					$error=1;
+					array_push($all_error_msg,['emp_code' => $emp_code->empcode, 'message' => $ee->getMessage()]);
+				}
+			}
+		if($error){
+			$this::mail_error($all_error_msg);
+		}
+	}
+	public function sendMail(){
 		$to_email="manish.dixit@rupeeboss.com";
 		$cc_email="";
 		$bcc_email="";
@@ -38,6 +55,67 @@ class SheduledTaskController extends Controller
 		  $error=2;
 		  echo $error;
 		}
-		print_r("hhhh");
 	}
+	public function mail_error($msg){
+		$data=DB::table('mail_error_log')->insert($msg);
+	}
+	public function get_managerwise_data($emp_code){
+		  try{
+		    $adapter = new Local(public_path('pdf'));
+		    $destinationPath = public_path(). '/pdf/';
+		    $file     =$req->file('attachment_path');
+
+		    if($file!=null || $file!=0){
+		      $fileName = rand(1, 999) . $file->getClientOriginalName();
+		      $filePath = "/temp/" . date("Y") . '/' . date("m") . "/" . $fileName;
+		      $file->move($destinationPath, $fileName);
+		    }else{
+		      $file=0;
+		    }
+		    $filename=date('Y-m-d-h-i-s'); 
+		    $data['15']=DB::select("call usp_get_lead_15daystat('".$emp_code."')");
+			$data['30']=DB::select("call usp_get_lead_30daystat('".$emp_code."')");
+			$data['45']=DB::select("call usp_get_lead_45daystat('".$emp_code."')");
+
+
+		  	foreach ($data as $key => $value) {
+		  		
+		   		PDF::loadView('downloadpdf-intimation-mail',['data'=>$value])->save($filename.".pdf",$adapter);
+		 	}
+
+	}
+
+
+	// public static function CallRaw($procName, $parameters = null, $isExecute = false)
+	// {
+	//     $syntax = '';
+	//     for ($i = 0; $i < count($parameters); $i++) {
+	//         $syntax .= (!empty($syntax) ? ',' : '') . '?';
+	//     }
+	//     $syntax = 'CALL ' . $procName . '(' . $syntax . ');';
+
+	//     $pdo = DB::connection()->getPdo();
+	//     $pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, true);
+	//     $stmt = $pdo->prepare($syntax,[\PDO::ATTR_CURSOR=>\PDO::CURSOR_SCROLL]);
+	//     for ($i = 0; $i < count($parameters); $i++) {
+	//         $stmt->bindValue((1 + $i), $parameters[$i]);
+	//     }
+	//     $exec = $stmt->execute();
+	//     if (!$exec) return $pdo->errorInfo();
+	//     if ($isExecute) return $exec;
+
+	//     $results = [];
+	//     do {
+	//         try {
+	//             $results[] = $stmt->fetchAll();
+	//             print_r($results);
+	//         } catch (\Exception $ex) {
+	//         	print_r( $ex->getMessage());exit();
+	//         }
+	//     } while ($stmt->nextRowset());
+
+
+	//     if (1 === count($results)) return $results[0];
+	//     return $results;
+	// }
 }
---
+++ resources/views/downloadpdf-intimation-mail.blade.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+ 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+
+  <body>
+ 
+ <div class="container" >
+ <br>
+    <nav class="navbar">
+    <div class="col-sm-12"> <p class="h2">LANDMARK INSURANCE PRIVATE LIMITED</p> <img src="http://localhost/corporate/public/images/landmark.png" width="80"  class="pull-right" style="margin-top:-50px; position: absolute; right:0px;z-index:1000;"> </div>
+     
+    </nav>
+      <div class="row">
+        <div class="col-md-10 ">
+          <h3 class="text-center"><strong>Target Intimation {{$value }}</strong> </h3>
+        </div>
+     <div class="col-md-12"> 
+    <label class="control-label col-sm-2"  > </label>
+    <div class="col-sm-8">
+   <table class="table table-bordered table-striped">
+    <thead>
+      <tr>
+        
+      </tr>
+    </thead>
+  </table>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
